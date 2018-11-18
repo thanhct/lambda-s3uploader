841b5b8903b84112f40d9bffa1c6c708a950583d
thanhsoftware <caothanhthanh@gmail.com>
2018-11-18 21:33:04 +0700

    write function to list object from s3 and return to client as array objects

--- list/handler.js
+++ list/handler.js
@@ -1,17 +1,56 @@
 'use strict';
 const aws = require('aws-sdk');
 const s3 = new aws.S3();
+const AWS_URL = 'http://thanhct-laravel.s3.amazonaws.com/';
 
-module.exports.listS3 = async (event, context) => {
-  console.log(s3);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Lists3',
-      input: event,
-    }),
+module.exports.listS3 = async (event, context, uploadCallback) => {
+  var params = {
+    Bucket: 'thanhct-laravel', /* required */
+    // Delimiter: 'STRING_VALUE',
+    // EncodingType: url,
+    // Marker: 'STRING_VALUE',
+    MaxKeys: 20,
+    // Prefix: 'STRING_VALUE',
+    // RequestPayer: requester
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return listObject(event, context, params, uploadCallback)
+      .then((data) => {
+        finishRequest(200, JSON.stringify(data) , 'application/json', uploadCallback);
+      })
+      .catch((err) => {
+        finishRequest(400, err, 'application/json', uploadCallback);
+  });
 };
+
+function listObject(event, context, params, uploadCallback) {
+  return new Promise((resolve, reject) => {
+    var listUrl = [];
+    s3.listObjectsV2(params, function(err, data) {
+       if (err) {
+         console.log(err, err.stack); // an error occurred
+         return reject(err);
+       } else {
+         var contents = data.Contents;
+         console.log(data.Contents);           // successful response
+         contents.forEach((element, index) => {
+           var objectUrl = AWS_URL + element.Key;
+           listUrl.push(objectUrl);
+         })
+         console.log('listUrl', JSON.stringify(listUrl));
+         resolve(listUrl);
+      };
+    });
+  });
+};
+
+function finishRequest(statusCode, body, contentType, uploadCallback, isBase64Encoded = false) {
+  uploadCallback(null, {
+    statusCode: statusCode,
+    isBase64Encoded: isBase64Encoded,
+    body: body,
+    headers: {
+      'Content-Type': contentType
+    }
+  });
+}
