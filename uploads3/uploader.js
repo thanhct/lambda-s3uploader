87e5247c9e51b225fcfeceb9cf93a5cbbc1554ad
thanhsoftware <caothanhthanh@gmail.com>
2018-12-03 23:07:14 +0700

    remove console log event

--- uploads3/uploader.js
+++ uploads3/uploader.js
@@ -6,8 +6,8 @@ const mime = require('mime-types');
 const path = require('path');
 const STORE_BUCKET = process.env.STORE_BUCKET;
 
-exports.handler = async (event) => (
-    console.log(event);
+exports.handler = async (event) => {
+    // console.log(event);
     // parse body to json
     let bodyJson = JSON.parse(event.body);
     let imageData = Buffer.from(bodyJson.file.replace(/^data:image\/\w+;base64,/, ""), 'base64');
@@ -32,7 +32,7 @@ exports.handler = async (event) => (
         "isBase64Encoded": false
     };
     return response;
-);
+};
 
 function putS3(params) {
     return new Promise((resolve, reject) => {
